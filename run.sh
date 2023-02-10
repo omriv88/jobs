bb66e6170876c3ae826e2a6845201b61348d5564
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-11 00:32:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ git clone https://github.com/omriv88/jobs.git
 rm -r -f /var/jenkins_home/jobs/*
 mkdir -p /var/jenkins_home/jobs
 cp -R -f /jobs/* /var/jenkins_home/jobs/
-cp -r -f /jobs/pipeline/config.xml /var/jenkins_home/jobs/config.xml
+#cp -r -f /jobs/pipeline/config.xml /var/jenkins_home/jobs/config.xml
