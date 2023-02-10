c7774a8fd3ac7a7c781e3da84c95083b2294cdab
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-10 16:45:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git clone https://github.com/omriv88/jobs.git
 mkdir /var/jenkins_home/jobs
-cp -r -f ./jobs/pipeline /var/jenkins_home/jobs/
-cp -r -f ./jobs/'pull request demo' /var/jenkins_home/jobs/
-#cp -r -f /var/jenkins_home/jobs/jobs/* /var/jenkins_home/jobs/
+cp -r -f ./jobs/* /var/jenkins_home/jobs/
