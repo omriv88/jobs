c3b321a1a46c7c11820e7c843a46353f87f6d78e
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-10 16:21:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/omriv88/jobs.git
 mkdir /var/jenkins_home/jobs
 cp -r -f ./jobs/* /var/jenkins_home/jobs/
-cp -r -f /var/jenkins_home/jobs/jobs/* /var/jenkins_home/jobs/
+#cp -r -f /var/jenkins_home/jobs/jobs/* /var/jenkins_home/jobs/
